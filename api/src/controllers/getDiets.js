5826f2dbc6a1325086a1dbf13f3666b71d44d373
Julian Gonzalez <julianmarcos505@gmail.com>
2022-03-24 23:18:43 -0300

    apikey fix

--- api/src/controllers/getDiets.js
+++ api/src/controllers/getDiets.js
@@ -1,15 +1,15 @@
 const axios = require("axios");
-const { Diet, RecipeAPI } = require("../db.js");
-const { APIKEY } = process.env;
+const { Diet, RecipeAPI} = require("../db.js");
+// const { APIKEY } = process.env;
 
 const getAllDiets = async () => {
-  // const apiInfo = await axios.get(
-  //   `https://api.spoonacular.com/recipes/complexSearch?apiKey=${APIKEY}&addRecipeInformation=true&number=100`
-  // );
-  // const apiDietsArrays = apiInfo.data.results.map((e) => e.diets);
+  const apiInfo = await axios.get(
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey='ca01b776ff2c4f4b861620d22d97b2fb'&addRecipeInformation=true&number=100`
+  );
+  const apiDietsArrays = apiInfo.data.results.map((e) => e.diets);
 
-  const apiInfo = await RecipeAPI.findAll();
-  const apiDietsArrays = apiInfo.map((e) => e.diets);
+  // const apiInfo = await RecipeAPI.findAll();
+  // const apiDietsArrays = apiInfo.map((e) => e.diets);
   
   const allApiTypes = apiDietsArrays.flat().concat("ketogenic", "vegetarian");
   await allApiTypes.forEach((diet) => {
